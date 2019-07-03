85b1ff91230bc0f0da6cafb5cf36514f090592c1
greti1234 <greticani@ymail.com>
2019-07-03 09:45:42 +0200

    Initial commit

---
+++ src/main/java/com/pagination/CustomerController.java
@@ -0,0 +1,35 @@
+package com.pagination;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+    private CustomerRepository customerRepository;
+    
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+    
+    @GetMapping("/customers")
+    public String customersPage(HttpServletRequest request, Model model) {
+        
+        int page = 0; //default page number is 0 (yes it is weird)
+        int size = 10; //default page size is 10
+        
+        if (request.getParameter("page") != null && !request.getParameter("page").isEmpty()) {
+            page = Integer.parseInt(request.getParameter("page")) - 1;
+        }
+
+        if (request.getParameter("size") != null && !request.getParameter("size").isEmpty()) {
+            size = Integer.parseInt(request.getParameter("size"));
+        }
+        
+        model.addAttribute("customers", customerRepository.findAll(PageRequest.of(page, size)));
+        return "customers";
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+insert into customer
+values('1','Name', 'Address');
+insert into customer
+values('2','Name', 'Address');
+insert into customer
+values('3','Name', 'Address');
+insert into customer
+values('4','Name', 'Address');
+insert into customer
+values('5','Name', 'Address');
+insert into customer
+values('6','Name', 'Address');
+insert into customer
+values('7','Name', 'Address');
+insert into customer
+values('8','Name', 'Address');
+insert into customer
+values('9','Name', 'Address');
+insert into customer
+values('10','Name', 'Address');
+insert into customer
+values('11','Name', 'Address');
+insert into customer
+values('12','Name', 'Address');
+insert into customer
+values('13','Name', 'Address');
+insert into customer
+values('14','Name', 'Address');
+insert into customer
+values('15','Name', 'Address');
+insert into customer
+values('16','Name', 'Address');
+insert into customer
+values('17','Name', 'Address');
+insert into customer
+values('18','Name', 'Address');
+insert into customer
+values('19','Name', 'Address');
+insert into customer
+values('20','Name', 'Address');
